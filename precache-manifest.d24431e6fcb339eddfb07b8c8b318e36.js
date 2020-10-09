f7ab6f6b3e2a057fa27645e3ddb39a8e2cae8217
Dang.Dinh.Luan <dang.dinh.luan@framgia.com>
2020-10-09 14:45:55 +0700

    Updates

---
+++ precache-manifest.d24431e6fcb339eddfb07b8c8b318e36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ae0a2f87b3b89e01fcf4ff20cb741f1",
+    "url": "/luanbkap1903.github.io/index.html"
+  },
+  {
+    "revision": "5eea053e48ab70a911a3",
+    "url": "/luanbkap1903.github.io/static/css/main.e90635fe.chunk.css"
+  },
+  {
+    "revision": "995163915616e84d8eb5",
+    "url": "/luanbkap1903.github.io/static/js/2.9518298d.chunk.js"
+  },
+  {
+    "revision": "569f33c8d1d96bc5b64a6ce10183922b",
+    "url": "/luanbkap1903.github.io/static/js/2.9518298d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5eea053e48ab70a911a3",
+    "url": "/luanbkap1903.github.io/static/js/main.acf39efa.chunk.js"
+  },
+  {
+    "revision": "0dd929131b9af6228587",
+    "url": "/luanbkap1903.github.io/static/js/runtime-main.5844a902.js"
+  }
+]);
