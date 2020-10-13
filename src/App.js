02d90998fe810cb7fa209a76bd4960f007dac435
Dang.Dinh.Luan <dang.dinh.luan@framgia.com>
2020-10-13 07:47:02 +0700

    use hashroute and change profile

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faTrash, faPlus, faEdit, faTimes, faBars } from "@fortawesome/free-solid-svg-icons";
 import { faFacebook, faTwitter, faInstagram, faLinkedin } from "@fortawesome/free-brands-svg-icons";
-import { BrowserRouter, Route } from "react-router-dom";
+import { HashRouter, Route } from "react-router-dom";
 library.add(faTrash, faEdit, faPlus, faBars, faTimes, faFacebook, faTwitter, faInstagram, faLinkedin);
 
 export default class App extends Component {
@@ -18,7 +18,7 @@ export default class App extends Component {
   render() {
     return (
       <div className="App">
-        <BrowserRouter>
+        <HashRouter>
           <SiteBar/>
           <div className="container-fluid">
             <main className="tm-main">
@@ -28,7 +28,7 @@ export default class App extends Component {
               </Route>
             </main>
           </div>
-        </BrowserRouter>
+        </HashRouter>
       </div>
     );
   }
