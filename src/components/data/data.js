a10d0ac63ffeabf713490796af5804a6fac14aac
Dang.Dinh.Luan <dang.dinh.luan@framgia.com>
2020-11-03 14:29:07 +0700

    post 4 react fragments

--- src/components/data/data.js
+++ src/components/data/data.js
@@ -1,7 +1,8 @@
 import bai1 from "./bai1.json";
 import bai2 from "./bai2.json";
 import bai3 from "./bai3.json";
+import bai4 from "./bai4.json";
 
-export const postsData = [bai3,bai2,bai1]
+export const postsData = [bai4,bai3,bai2,bai1]
 
 export default postsData
